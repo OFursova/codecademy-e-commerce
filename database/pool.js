5b293069e42758bc8f0ae15a23212f640b7ce6f8
Olga Fursova <olga.fursova.zp@gmail.com>
2024-02-27 21:13:28 +0200

    fixed password hashing

--- database/pool.js
+++ database/pool.js
@@ -1,12 +1,11 @@
 const { Pool } = require('pg');
 
-// PostgreSQL connection pool
 const pool = new Pool({
     user: 'olga',
     host: 'localhost',
     database: 'store-project',
     password: 'admin',
-    port: 5432, // default PostgreSQL port
+    port: 5432,
 });
 
 module.exports = pool;
